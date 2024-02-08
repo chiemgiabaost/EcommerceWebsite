43c82f5d8b2a05400cf6c08214935ce263d534ec
chiemgiabaost <bao2003@my.yorku.ca>
2024-02-07 23:18:12 -0500

    Updaate work

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
-
+import {Link, NavLink} from 'react-router-dom'
+import {SiShopware} from "react-icons/si"
+import { MdOutlineCancel } from "react-icons/md";
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import {links} from "../data/dummy"
+import { useStateContext} from '../Contexts/ContextProvider';
 const Sidebar = () => {
+  const {activeMenu, setActiveMenu, screenSize} = useStateContext();
+  const handleCloseSidebar = () => {
+    if ( activeMenu && screenSize <=900){
+      setActiveMenu(false);
+    }
+  }
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-white m-2'
+  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2'
+
   return (
-    <div>Sidebar</div>
+    <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
+      {activeMenu && (
+        <>
+          <div className='flex justify-between items-center'>
+            <Link className="items-center gap-3 ml-3 mt-3 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
+             to="/" onClick={(prevActivemenu) => setActiveMenu(!prevActivemenu)}>
+              <SiShopware></SiShopware> <span>Shoppy</span>
+            </Link>
+            <TooltipComponent content="Menu" position='BottomCenter'>
+              <button className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block " type='button' onClick={() => {}}>
+                <MdOutlineCancel/>
+              </button>
+            </TooltipComponent>
+          </div>
+          <div className='mt-10'>
+            {links.map((item,index) => (
+              <div className='' key={item.title}>
+                <p className='text-gray-400 m-3 mt-4 uppercase'>
+                  {item.title}
+                </p>
+                {item.links.map((link,index) => (
+                  <NavLink
+                  to={`/${link.name}`}
+                  key={link.name}
+                  onClick={() => {handleCloseSidebar()}}
+                  
+                  className={({ isActive }) => (isActive ? activeLink : normalLink)}
+                >
+                  {link.icon}
+                  <span className="capitalize ">{link.name}</span>
+                </NavLink>
+                ))}
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-
+import { ContextProvider } from './Contexts/ContextProvider';
 ReactDOM.render(
   
-      <App />
+  <ContextProvider> <App /></ContextProvider>
   ,
   document.getElementById('root'),
 );
